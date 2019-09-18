2f9f1059adb24082a14345908a2a44a0eec57e16
asphy <asphy@oreo>
2019-09-18 10:20:30 -0400

    bruh?

--- unit2/NamesTester.java
+++ unit2/NamesTester.java
@@ -1,7 +1,24 @@
-import java.util.Scanner;
+import java.util.*;
 
 class NamesTester{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
+        ArrayList<Name> names = new ArrayList<Name>();
+        int namecount = 3;
+        for(int i=0;i<namecount;i++){
+            System.out.print("Enter first name: ");
+            String first = input.nextLine();
+            System.out.print("Enter middle name: ");
+            String middle = input.nextLine();
+            System.out.print("Enter last name: ");
+            String last = input.nextLine();
+            Name tempName = new Name(first,middle,last);
+            names.add(tempName);
+        }
+        for(Name n : names)
+            System.out.print(n.firstMiddleLast()+"\n"+n.lastFirstMiddle()+"\nInitials: "+n.initials()+"\nLength: "+n.length());
+        for(Name n : names)
+            for(int i=0;i<names.size();i++)
+                n.equals(names.get(i));
     }
 }
