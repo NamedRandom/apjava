bd992d636fda4a03b7552c8ad19c73ebd7559beb
asphy <asphy@oreo>
2019-10-23 11:26:03 -0400

    finished DNA, it look cool

--- unit4/DNA.java
+++ unit4/DNA.java
@@ -1,25 +1,26 @@
 import java.util.*;
 
 class DNA{
-    static final int length=10;
-    static final int mismatch=3;
+
+    static final int mismatch=0;
 
     public static void main(String[] args){
 
         Scanner input = new Scanner(System.in);
 
-        char[] dna1 = new char[length];
-        char[] dna2 = new char[length];
+        char[] dna1,dna2;
 
-        System.out.println("Enter DNA1:");
+        System.out.print("Enter DNA1:");
         dna1=input.nextLine().toCharArray();
-        System.out.println("Enter DNA2:");
+        System.out.print("Enter DNA2:");
         dna2=input.nextLine().toCharArray();
         System.out.println("You entered:\nDNA1: "+Arrays.toString(dna1)+"\nDNA2: "+Arrays.toString(dna2));
-/*
-        dna1 = new char[]{'A','C','T','C','T','T','A','G','G'};
-        dna2 = new char[]{'G','C','C','C','T','T','A','G','G'};
-*/
+
+        if(dna1.length!=dna2.length){
+            System.out.println("Length mismatch!");
+            return;
+        }
+
         int mismatchCount=0;
         int backMismatchCount=0;
 
@@ -27,10 +28,10 @@ class DNA{
             if(dna1[i]!=dna2[i]) mismatchCount++;
         }
         for(int i=dna1.length-1;i>=0;i--){
-            if(dna1[i]!=dna2[i]) backMismatchCount++;
+            if(dna1[i]!=dna2[dna2.length-1-i]) backMismatchCount++;
         }
-        if(mismatchCount>mismatch&&backMismatchCount>mismatch) System.out.println("DNA1 does not match with DNA2");
-        if(mismatchCount<=mismatch) System.out.println("DNA1 matches with DNA2 forward with "+mismatchCount+" mismatches ("+mismatch+" mismatches allowed)");
-        if(backMismatchCount<=mismatch) System.out.println("DNA1 matches with DNA2 backwards with "+backMismatchCount+" mismatches ("+mismatch+" mismatches allowed)");
+        if(mismatchCount>mismatch&&backMismatchCount>mismatch) System.out.println("DNA1 does not match with DNA2, "+mismatchCount+" mismatches forward, "+backMismatchCount+" mismatches backward");
+        if(mismatchCount<=mismatch) System.out.println("DNA1 matches with DNA2 forward with "+mismatchCount+" mismatches out of "+mismatch);
+        if(backMismatchCount<=mismatch) System.out.println("DNA1 matches with DNA2 forward with "+backMismatchCount+" mismatches out of "+mismatch);
     }
 }
