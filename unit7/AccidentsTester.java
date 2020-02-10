f8bb5ff4c92fdb9c200ca70c8c854da0d91867f9
asphy <asphy@oreo>
2020-02-10 10:36:13 -0500

    finish accidents

---
+++ unit7/AccidentsTester.java
@@ -0,0 +1,11 @@
+class AccidentsTester{
+    public static void main(String[] args){
+        Accidents a = new Accidents();
+
+        a.printAccidents();
+
+        a.totalAccidents();
+
+        a.mostAccidentsHour();
+    }
+}
