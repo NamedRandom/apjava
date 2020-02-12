0c5f6894c720e82f76c3f48452326247da0670d4
asphy <asphy@oreo>
2020-02-12 10:24:58 -0500

    finished OrderMatters

--- unit7/OrderMatters.java
+++ unit7/OrderMatters.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 
 class OrderMatters {
   private double[][] orderedData;
+  public boolean slow;
   private int rows;
   private int cols;
   private DecimalFormat df = new DecimalFormat("#.##");
@@ -13,18 +14,68 @@ class OrderMatters {
     orderedData = new double[rows][cols];
   }
 
-  public void rowOrder(double[] arr) {
+  public void rowOrder(double[] arr) throws InterruptedException {
     Arrays.sort(arr);
     int rowIndex = 0;
     int colIndex = 0;
     int readIndex = 0;
-    while (rowIndex < orderedData.length && readIndex < arr.length) {
+    while (readIndex < arr.length) {
       if (colIndex == orderedData[0].length) {
         colIndex = 0;
         rowIndex++;
       }
       orderedData[rowIndex][colIndex++] = arr[readIndex++];
+      if (slow) slowPrint();
     }
+    if (slow) Thread.sleep(2000);
+  }
+
+  public void columnOrder(double[] arr) throws InterruptedException {
+    Arrays.sort(arr);
+    int rowIndex = 0;
+    int colIndex = 0;
+    int readIndex = 0;
+    while (readIndex < arr.length) {
+      if (rowIndex == orderedData.length) {
+        rowIndex = 0;
+        colIndex++;
+      }
+      orderedData[rowIndex++][colIndex] = arr[readIndex++];
+      if (slow) slowPrint();
+    }
+    if (slow) Thread.sleep(2000);
+  }
+
+  public void wrappedOrder(double[] arr) throws InterruptedException {
+    Arrays.sort(arr);
+    int rowIndex = 0;
+    int colIndex = 0;
+    int readIndex = 0;
+    boolean forward = true;
+    while (readIndex < arr.length - 1) {
+      orderedData[rowIndex][colIndex] = arr[readIndex];
+      if (readIndex != arr.length - 2) {
+        if (colIndex == 0 && readIndex > 0) {
+          forward = true;
+          rowIndex++;
+          readIndex++;
+          orderedData[rowIndex][colIndex] = arr[readIndex];
+        } else if (colIndex == orderedData[0].length - 1) {
+          forward = false;
+          rowIndex++;
+          readIndex++;
+          orderedData[rowIndex][colIndex] = arr[readIndex];
+        }
+      } else if (forward) orderedData[rowIndex][colIndex + 1] = arr[readIndex + 1];
+      else orderedData[rowIndex][colIndex - 1] = arr[readIndex + 1];
+
+      if (forward) colIndex++;
+      else colIndex--;
+
+      readIndex++;
+      if (slow) slowPrint();
+    }
+    if (slow) Thread.sleep(2000);
   }
 
   public void print2D() {
@@ -37,5 +88,16 @@ class OrderMatters {
         System.out.print("\t" + df.format(orderedData[i][j]));
       System.out.println();
     }
+  }
+
+  private void slowPrint() throws InterruptedException {
+    Thread.sleep(250);
+    System.out.print("\033\143");
+    print2D();
+  }
+
+  public void clear() {
+    for (int i = 0; i < orderedData.length; i++)
+      for (int j = 0; j < orderedData[0].length; j++) orderedData[i][j] = 0;
   }
 }
--- unit7/OrderMattersTest.java
+++ unit7/OrderMattersTest.java
@@ -1,28 +1,29 @@
 import java.util.Random;
 
 class OrderMattersTest {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws InterruptedException {
 
     Random rng = new Random();
 
-    int arr1row = rng.nextInt(5) + 5;
-    int arr1col = rng.nextInt(5) + 5;
-    double[] arr1 = new double[arr1row * arr1col];
-    int arr2row = rng.nextInt(5) + 5;
-    int arr2col = rng.nextInt(5) + 5;
-    double[] arr2 = new double[arr2row * arr2col];
-    int arr3row = rng.nextInt(5) + 5;
-    int arr3col = rng.nextInt(5) + 5;
-    double[] arr3 = new double[arr3row * arr3col];
-    for (int i = 0; i < arr1.length; i++) arr1[i] = (rng.nextDouble() + 1) * arr1.length;
-    for (int i = 0; i < arr2.length; i++) arr2[i] = (rng.nextDouble() + 2) * arr2.length;
-    for (int i = 0; i < arr3.length; i++) arr3[i] = (rng.nextDouble() + 3) * arr3.length;
-
-    OrderMatters a1 = new OrderMatters(arr1row, arr1col);
-    OrderMatters a2 = new OrderMatters(arr2row, arr2col);
-    OrderMatters a3 = new OrderMatters(arr3row, arr3col);
-
-    a1.rowOrder(arr1);
-    a1.print2D();
+    for (int i = 0; i < 3; i++) {
+      int row = rng.nextInt(5) + 5;
+      int col = rng.nextInt(5) + 5;
+      double[] arr = new double[row * col];
+      for (int j = 0; j < arr.length; j++) arr[j] = j;
+      OrderMatters order = new OrderMatters(row, col);
+      order.slow = true;
+      System.out.println("Row order:");
+      order.rowOrder(arr);
+      order.print2D();
+      order.clear();
+      System.out.println("Column order:");
+      order.columnOrder(arr);
+      order.print2D();
+      order.clear();
+      System.out.println("Wrapped order:");
+      order.wrappedOrder(arr);
+      if (!order.slow) order.print2D();
+      order.clear();
+    }
   }
 }
